4e1ac22f537febcf3447323e68826acb47122cb4
Eren <erenada@gmail.com>
2021-08-17 17:31:50 -0400

    finally?

--- CONTIGS/run_raxml.sh
+++ CONTIGS/run_raxml.sh
@@ -8,12 +8,13 @@
 #SBATCH --exclusive
 #SBATCH -o run_raxml.out
 #SBATCH -e run_raxml.err
-#SBATCH --export=tree=$1
-#SBATCH --export=alignFolder=$2 #path to the alignment folder
-#SBATCH --export=outFolder=$3 #path to the output folder
-#SBATCH --export=randomcount=$4 #only in use for randomly selecting contigs - change for loop, give a numer eg: 100
 
-submit_directory=$SLURM_SUBMIT_DIR
+var0=$SLURM_SUBMIT_DIR
+var1="$var0/JarvisFinalTree.nwk" #tree file
+var2="/data/schwartzlab/eren/Chapter2/SISRS_Run/aligned_contigs" #input contigs folder
+var3="$var0/raxml_out" #output folder
+var4="10" #alignment number
+
 
 #module load RAxML/8.2.12-intel-2019b-hybrid-avx2
 
@@ -24,18 +25,18 @@ submit_directory=$SLURM_SUBMIT_DIR
 # sbatch run_raxml.sh JarvisFinalTree.nwk /data/schwartzlab/eren/Chapter2/SISRS_Run/aligned_contigs /raxml_out 100
 
 
-for alignment in $(ls $alignFolder | sort -R | tail -$randomcount);
+for alignment in $(ls $var2 | sort -R | tail -$randomcount);
 do
 contigName=$(echo "$alignment" | cut -d "-" -f 2 | cut -d "." -f 1)
 #for alignment in $(ls $alignFolder/*fasta); # go through each fasta file
-/data/schwartzlab/eren/programs/standard-RAxML/raxmlHPC-PTHREADS-SSE3 -f e -t $submit_directory/$1 -m GTRGAMMA -s $alignFolder/$alignment -n "$contigName" -T 10
+/data/schwartzlab/eren/programs/standard-RAxML/raxmlHPC-PTHREADS-SSE3 -f e -t $var1 -m GTRGAMMA -s $var2/$alignment -n "$contigName" -T 10
 done
 
 #name change for tree files
 
-mv $submit_directory/RaxML_* $submit_directory/$outFolder
+mv $var0/RaxML_* $var3/
 
-cd $submit_directory/$outFolder
+cd $var0/$var3
 
 for tree in $(ls *result*);
 do
