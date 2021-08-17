1335a7a89fd27e334623bdd320b60b52c90d0be0
Eren <erenada@gmail.com>
2021-08-17 16:42:52 -0400

    path-fix

--- CONTIGS/run_raxml.sh
+++ CONTIGS/run_raxml.sh
@@ -21,6 +21,7 @@ tree=$1 #path to the tree file
 alignFolder=$2 #path to the alignment folder
 outFolder=$3 #path to the output folder
 randomcount=$4 #only in use for randomly selecting contigs - change for loop, give a numer eg: 100
+submit_directory=$(pwd) #change in bash
 
 cd $alignFolder
 
@@ -28,7 +29,7 @@ for alignment in $(ls $alignFolder | sort -R | tail -$randomcount);
 do
 contigName=$(echo "$alignment" | cut -d "-" -f 2 | cut -d "." -f 1)
 #for alignment in $(ls $alignFolder/*fasta); # go through each fasta file
-/data/schwartzlab/eren/programs/standard-RAxML/raxmlHPC-PTHREADS-SSE3 -f e -t $1 -m GTRGAMMA -s $alignFolder/$alignment -n "$contigName" -T 10
+/data/schwartzlab/eren/programs/standard-RAxML/raxmlHPC-PTHREADS-SSE3 -f e -t $submit_directory/$1 -m GTRGAMMA -s $alignFolder/$alignment -n "$contigName" -T 10
 done
 
 #name change for tree files
