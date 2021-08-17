490bfedd9ac96a7eb3cc092047ee76dd0f01ead4
Eren <erenada@gmail.com>
2021-08-17 15:42:59 -0400

    example_command

--- CONTIGS/run_raxml.sh
+++ CONTIGS/run_raxml.sh
@@ -9,19 +9,24 @@
 #SBATCH -o run_raxml.out
 #SBATCH -e run_raxml.err
 
+module load RAxML/8.2.12-intel-2019b-hybrid-avx2
+
 #this script runs raxml for each alignment on a reference tree
 
 #usage: sbatch run_raxml.sh </tree.nwk> </alignFolder> <outFolder> <randomcount>
 
+# sbatch run_raxml.sh RefTree/JarvisFinalTree.nwk /data/schwartzlab/eren/Chapter2/SISRS_Run/aligned_contigs /raxml_out 100
+
 tree=$1 #path to the tree file
 alignFolder=$2 #path to the alignment folder
 outFolder=$3 #path to the output folder
-randomcount=$4 #only in use for randomly selecting contigs - change for loop, give a numer eg: 100,
+randomcount=$4 #only in use for randomly selecting contigs - change for loop, give a numer eg: 100
 
 for alignment in $(ls $alignFolder/*fasta | sort -R | tail -$randomcount);
+do
+contigName=$(echo "$alignment" | cut -d "-" -f 2 | cut -d "." -f 1)
 #for alignment in $(ls $alignFolder/*fasta); # go through each fasta file
 do
-  contigName=$(echo "$alignment" | cut -d "-" -f 2 | cut -d "." -f 1)
 raxmlHPC -f e -t $1 -m GTRGAMMA -s $alignment -n "$contigName"
 done
 
