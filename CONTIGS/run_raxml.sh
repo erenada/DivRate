a3bd9ce8dc1d74bd8fe866a34f63ad80a4593e7c
Eren <erenada@gmail.com>
2021-08-17 16:50:28 -0400

    sbatch ready

--- CONTIGS/run_raxml.sh
+++ CONTIGS/run_raxml.sh
@@ -21,7 +21,7 @@ tree=$1 #path to the tree file
 alignFolder=$2 #path to the alignment folder
 outFolder=$3 #path to the output folder
 randomcount=$4 #only in use for randomly selecting contigs - change for loop, give a numer eg: 100
-submit_directory=$(pwd) #change in bash
+submit_directory=$SLURM_SUBMIT_DIR #change in bash
 
 cd $alignFolder
 
