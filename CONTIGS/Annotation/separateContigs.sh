ccf5f2e3ecbf3350e8bebbf12f23be49e0d09cbe
Eren <erenada@gmail.com>
2021-08-31 13:55:57 -0400

    sepcontigs

--- CONTIGS/Annotation/separateContigs.sh
+++ CONTIGS/Annotation/separateContigs.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-cd /data/schwartzlab/eren/Chapter1/CONTIGS/Annotation
+cd /data/schwartzlab/eren/Chapter2/CONTIGS/Annotation
 
-listOfData=$(cat annot.csv | tail -n +2 | cut -f 2 | sort | uniq)
+listOfData=$(cat annotations.csv | tail -n +2 | cut -f 2 | sort | uniq)
 
 for type in $listOfData;
 do
   mkdir $type
 
-  alignmentList=$(tail -n +2 /data/schwartzlab/eren/Chapter1/CONTIGS/Annotation/"$type"_table.csv | cut -f 1)
+  alignmentList=$(tail -n +2 /data/schwartzlab/eren/Chapter2/CONTIGS/Annotation/"$type"_table.csv | cut -f 1)
   for file in $alignmentList;
   do
-    ln -s /data/schwartzlab/eren/Chapter1/CONTIGS/contigs_5_missing/${file} /data/schwartzlab/eren/Chapter1/CONTIGS/Annotation/${type}/${file}
+    ln -s /data/schwartzlab/eren/Chapter2/CONTIGS/contigs_5_missing/${file} /data/schwartzlab/eren/Chapter2/CONTIGS/Annotation/${type}/${file}
   done
 done
