4ec6ba209874dc5428987f293afc5b354305f5b6
Eren <erenada@gmail.com>
2021-08-31 15:37:37 -0400

    align_subsets

---
+++ CONTIGS/Annotation/align_subsets.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --job-name="align_subsets"
+#SBATCH --time=99:00:00  # walltime limit (HH:MM:SS)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --ntasks-per-node=20   # CHANGE THIS to processor core(s) per node
+#SBATCH --mail-user="erenada@uri.edu" #CHANGE THIS to your user email address
+#SBATCH --mail-type=END,FAIL
+#SBATCH -o %x_%A.out
+#SBATCH -e %x_%A.err
+#SBATCH --exclusive
+
+module load MAFFT/7.475-gompi-2020b-with-extensions
+
+listOfData=$(cat /data/schwartzlab/eren/Chapter2/CONTIGS/Annotation/annot_table_all.csv | tail -n +2 | cut -f 2 | sort | uniq)
+PTH=(/data/schwartzlab/eren/Chapter2/CONTIGS/Annotation/)
+PTH_OUT=(/data/schwartzlab/eren/Chapter2/CONTIGS/Annotation/ALIGNED)
+
+for type in $listOfData;
+do
+  mkdir /data/schwartzlab/eren/Chapter2/CONTIGS/Annotation/ALIGNED/${type}
+  FILELIST=( $( find $PTH/${type} -maxdepth 1 -type f ) )
+  ARRLEN=${#FILELIST[@]}
+  for (( i = 0; i < $ARRLEN; i++ ));
+  do
+    out_file="$(basename ${FILELIST[i]})"
+    mafft --nwildcard --auto --thread 20 ${FILELIST[i]} > $PTH/${type}/${out_file}
+  done
+done
