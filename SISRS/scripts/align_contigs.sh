9d2851583c5c5ac3f2ed01186081a5cf32a0ab06
Eren <erenada@gmail.com>
2021-09-01 14:36:26 -0400

    align_all

--- SISRS/scripts/align_contigs.sh
+++ SISRS/scripts/align_contigs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name="7b"
-#SBATCH --time=2:00:00  # walltime limit (HH:MM:SS)
+#SBATCH --time=120:00:00  # walltime limit (HH:MM:SS)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --ntasks-per-node=20   # processor core(s) per node
-#SBATCH --mail-user="yana_hrytsenko@uri.edu"
+#SBATCH --mail-user="erenada@uri.edu"
 #SBATCH --mail-type=END,FAIL
 
 cd $SLURM_SUBMIT_DIR
