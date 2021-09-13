43a0c3154995ce4d383d237f4e0ce2b44658b00a
Eren <erenada@gmail.com>
2021-09-13 16:03:55 -0400

    align_seperately

---
+++ CONTIGS/Annotation/Scripts/unannot_only_align.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name="align_unan2"
+#SBATCH --time=99:00:00  # walltime limit (HH:MM:SS)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --ntasks-per-node=20   # CHANGE THIS to processor core(s) per node
+#SBATCH --mail-user="erenada@uri.edu" #CHANGE THIS to your user email address
+#SBATCH --mail-type=END,FAIL
+#SBATCH -o %x_%A.out
+#SBATCH -e %x_%A.err
+#SBATCH --exclusive
+
+
+
+
+module load MAFFT/7.475-gompi-2020b-with-extensions
+
+
+type=$(cat /data/schwartzlab/eren/Chapter2/CONTIGS/Annotation/Tables/annot_table_all.csv | tail -n +2 | cut -f 2 | sort | uniq | grep unannotated)
+PTH=(/data/schwartzlab/eren/Chapter2/CONTIGS/Annotation/NOT_ALIGNED)
+PTH_OUT=(/data/schwartzlab/eren/Chapter2/CONTIGS/Annotation/ALIGNED)
+
+for file in $(find ${PTH}/${type} -name "*.fasta");
+do
+  out_file=$(basename ${file})
+  mafft --nwildcard --auto --thread 20 ${file} > ${PTH_OUT}/${type}/${out_file}
+done
