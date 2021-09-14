9b278c68e8130ba30ad922c1d1948dac7249f5fc
Eren <erenada@gmail.com>
2021-09-14 12:29:14 -0400

    linking_aligned_50K

--- CONTIGS/scripts/pickContigs.sh
+++ CONTIGS/scripts/pickContigs.sh
@@ -5,7 +5,7 @@
 
 for contig in $(cut -d'/' -f 1 /data/schwartzlab/eren/Chapter2/SISRS_Run/missing_5/alignment_locs_m5_nogap.txt | uniq -c | sort -bgr | sed 's/[[:space:]]*[[:digit:]]*[[:space:]]//' | head -50000);
 do
-  cp /data/schwartzlab/eren/Chapter2/CONTIGS/contigs_29M/$contig.fasta /data/schwartzlab/eren/Chapter2/CONTIGS/contigs_5M/
+  ln -s /data/schwartzlab/eren/Chapter2/SISRS_Run/aligned_contigs/"${contig}".fasta /data/schwartzlab/eren/Chapter2/CONTIGS/Annotation/ALIGNED/ALL_50K/"${contig}".fasta
 done
 
 
