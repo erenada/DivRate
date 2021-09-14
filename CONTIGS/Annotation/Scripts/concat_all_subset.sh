69e484f3e0df52bc3b2223ebb7952fe08a3b6d28
Eren <erenada@gmail.com>
2021-09-14 11:31:02 -0400

    concat_sub

--- CONTIGS/Annotation/Scripts/concat_all_subset.sh
+++ CONTIGS/Annotation/Scripts/concat_all_subset.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name="cat_subsets"
+#SBATCH --job-name="cat_subsets2"
 #SBATCH --time=100:00:00  # walltime limit (HH:MM:SS)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --ntasks-per-node=20   # CHANGE THIS to the number of processors on your node
@@ -20,8 +20,8 @@ module load Python/3.7.4-GCCcore-8.3.0
 
 listOfData=$(cat ../Tables/annot_table_all.csv | tail -n +2 | cut -f 2 | sort | uniq)
 
-PTH=(/data/schwartzlab/eren/Chapter1/CONTIGS/Annotation/ALIGNED)
-PTH_OUT=(/data/schwartzlab/eren/Chapter1/CONTIGS/Annotation/CONCAT)
+PTH=(/data/schwartzlab/eren/Chapter2/CONTIGS/Annotation/ALIGNED)
+PTH_OUT=(/data/schwartzlab/eren/Chapter2/CONTIGS/Annotation/CONCAT)
 
 for type in $listOfData;
 do
@@ -31,13 +31,13 @@ done
 
 for type in $listOfData;
 do
-  cp /data/schwartzlab/eren/Chapter1/CONTIGS/Annotation/Scripts/AMAS_ch1.py ${PTH}/${type}/AMAS_ch1.py
+  cp /data/schwartzlab/eren/Chapter2/CONTIGS/Annotation/Scripts/AMAS_ch2.py ${PTH}/${type}/AMAS_ch2.py
 
   cd ${PTH}/${type}
 
-  python3 AMAS_ch1.py concat -f fasta -d dna -i *fasta -c 20 --part-format raxml --concat-part "${type}"_partition.txt --concat-out "${type}"_concat.fasta
+  python3 AMAS_ch2.py concat -f fasta -d dna -i *fasta -c 20 --part-format raxml --concat-part "${type}"_partition.txt --concat-out "${type}"_concat.fasta
 
   mv "${type}"_concat.fasta ${PTH_OUT}/${type}/"${type}"_concat.fasta
   mv "${type}"_partition.txt ${PTH_OUT}/${type}/"${type}"_partition.txt
-  rm AMAS_ch1.py
+  rm AMAS_ch2.py
 done
